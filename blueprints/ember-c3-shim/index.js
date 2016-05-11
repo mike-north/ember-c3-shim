164bfd9b675265e1829c4d0eb54b1ead3e0c28a8
Mike North <michael.l.north@gmail.com>
2016-05-11 09:17:29 -0700

    Fastboot compatibility

--- blueprints/ember-c3-shim/index.js
+++ blueprints/ember-c3-shim/index.js
@@ -5,7 +5,7 @@ module.exports = {
 
   afterInstall: function(options) {
     return this.addBowerPackageToProject('c3', '~0.4.10').then(function () {
-      return this.addAddonToProject('ember-d3');
+      return this.addAddonToProject('ember-d3@0.2.0');
     }.bind(this));
   }
 };
