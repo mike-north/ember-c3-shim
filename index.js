cdf3f0c2c02da44be2b201666d191aa08e476fee
Khadim Fall <khadim.fall@alyne.com>
2018-08-15 11:12:30 +0200

    get rid of bower

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ module.exports = {
   name: 'c3',
   included: function included(app) {
     if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      app.import(`${app.bowerDirectory  }/c3/c3.min.js`);
-      app.import(`${app.bowerDirectory  }/c3/c3.min.css`);
+      app.import(`node_modules/c3/c3.min.js`);
+      app.import(`node_modules/c3/c3.min.css`);
       app.import('vendor/ember-c3.js', {
         exports: {
           c3: ['default']
